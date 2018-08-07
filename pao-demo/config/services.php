6011eebce863aef5410ade95f34b6aff668fe874
Paulius Opanovičius <paulius.opanovicius@gmail.com>
2018-08-07 10:03:36 +0300

    Elevate redirect url to config paramter.

--- pao-demo/config/services.php
+++ pao-demo/config/services.php
@@ -37,6 +37,6 @@ return [
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),         // Your GitHub Client ID
         'client_secret' => env('GITHUB_CLIENT_SECRET'), // Your GitHub Client Secret
-        'redirect' => 'http://testio.test/login/github/callback',
+        'redirect' => env('GITHUB_CLIENT_REDIRECT'),    // Redirect user back to your site
     ],
 ];
